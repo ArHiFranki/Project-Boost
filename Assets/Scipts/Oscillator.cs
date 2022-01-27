8b91bbd07b298df3feebc831ee17f500893405d5
ArHiFranki <franki550@gmail.com>
2022-01-27 14:30:48 +0700

    Добавил скрипт для циклического передвижения объектов с настраиваемыми параметрами

---
+++ Assets/Scipts/Oscillator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] private Vector3 movementVector;
+    [SerializeField] private float period = 2f;
+
+    private float movementFactor;
+    private Vector3 startingPosition;
+
+    private void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = (rawSinWave + 1f) / 2f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
